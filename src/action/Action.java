34f1ddb6b54ab88f395f86f9506572f32f5846cf
ph0513 <ph0513@naver.com>
2020-03-11 11:06:18 +0900

    Action 추가

--- src/action/Action.java
+++ src/action/Action.java
@@ -1,5 +1,8 @@
 package action;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public interface Action {
-	
+	public ActionFoward excute(HttpServletRequest req, HttpServletResponse res);
 }
